3dbd7ccb4d187d7b92f40468103331e205409d76
Nicolas Vega <nicolasvegaguerrero10@gmail.com>
2025-04-14 20:57:58 -0300

    se añade rsa

--- database/connection.py
+++ database/connection.py
@@ -2,10 +2,8 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
-# Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
-# Obtener las credenciales desde las variables de entorno
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
